928aad1e5d2ba060a320989691119f4927952fc8
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-18 20:59:52 +0200

    feat: add BooleanNode

---
+++ src/node/nodes/boolean_node.rs
@@ -0,0 +1,38 @@
+use crate::node::{
+    json_particle::JsonParticle,
+    nodes::json_node::JsonNode,
+    stringify_options::StringifyOptions
+};
+
+pub struct BooleanNode {
+    value: bool
+}
+
+impl BooleanNode {
+    pub fn has_value(&self, value: bool) -> bool {
+        self.value == value
+    }
+}
+
+impl JsonNode for BooleanNode {
+
+}
+
+impl JsonParticle for BooleanNode {
+    fn stringify_with_options(&self, _options: StringifyOptions) -> String {
+        self.value.to_string()
+    }
+}
+
+pub const TRUE: BooleanNode = BooleanNode {
+    value: true
+};
+
+pub const FALSE: BooleanNode = BooleanNode {
+    value: false
+};
+
+pub const BOOLEAN_NODES: [BooleanNode; 2] = [
+    TRUE,
+    FALSE
+];
--- src/node/nodes/mod.rs
+++ src/node/nodes/mod.rs
@@ -1,2 +1,3 @@
+pub mod boolean_node;
 pub mod json_node;
 pub mod null_node;
