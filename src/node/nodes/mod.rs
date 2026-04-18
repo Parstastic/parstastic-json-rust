a2b494e98879c7bf97cff84c6e657226b354d884
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-18 23:27:10 +0200

    feat: add ObjectNode

--- src/node/nodes/mod.rs
+++ src/node/nodes/mod.rs
@@ -4,4 +4,5 @@ pub mod container_node;
 pub mod json_node;
 pub mod null_node;
 pub mod number_node;
+pub mod object_node;
 pub mod string_node;
---
+++ src/node/nodes/object_node.rs
@@ -0,0 +1,89 @@
+use crate::node::{
+    json_particle::JsonParticle,
+    json_value::JsonValue,
+    nodes::{
+        container_node::{
+            ContainerNode,
+            ContainerNodeValue
+        },
+        json_node::JsonNode,
+        string_node::StringNode
+    },
+    stringify_options::StringifyOptions,
+    whitespace::Whitespace
+};
+
+pub const KEY_VALUE_DELIMITER: char = ':';
+
+pub struct ObjectNodeProperty {
+    leading_whitespace: Whitespace,
+    key: StringNode,
+    trailing_whitespace: Whitespace,
+    value: JsonValue,
+}
+
+impl ObjectNodeProperty {
+    pub fn new(
+        leading_whitespace: Whitespace,
+        key: StringNode,
+        trailing_whitespace: Whitespace,
+        value: JsonValue,
+    ) -> Self {
+        Self {
+            leading_whitespace,
+            key,
+            trailing_whitespace,
+            value,
+        }
+    }
+}
+
+impl JsonParticle for ObjectNodeProperty {
+    fn stringify_with_options(&self, options: StringifyOptions) -> String {
+        todo!()
+    }
+}
+
+
+pub const DELIMITER_START: char = '{';
+pub const DELIMITER_END: char = '}';
+pub const DELIMITER_ELEMENTS: char = ',';
+
+
+pub struct ObjectNode {
+    container_node: ContainerNode<ObjectNodeProperty>
+}
+
+impl ObjectNode {
+    pub fn new_with_whitespace(whitespace: Whitespace) -> Self {
+        Self {
+            container_node: ContainerNode::new(
+                ContainerNodeValue::Whitespace(whitespace),
+                DELIMITER_START,
+                DELIMITER_END,
+                DELIMITER_ELEMENTS
+            )
+        }
+    }
+
+    pub fn new_with_elements(elements: Vec<ObjectNodeProperty>) -> Self {
+        Self {
+            container_node: ContainerNode::new(
+                ContainerNodeValue::Elements(elements),
+                DELIMITER_START,
+                DELIMITER_END,
+                DELIMITER_ELEMENTS
+            )
+        }
+    }
+}
+
+impl JsonNode for ObjectNode {
+
+}
+
+impl JsonParticle for ObjectNode {
+    fn stringify_with_options(&self, options: StringifyOptions) -> String {
+        self.container_node.stringify_with_options(options)
+    }
+}
