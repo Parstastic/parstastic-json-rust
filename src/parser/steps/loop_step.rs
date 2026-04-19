b50e5ea79fa7427191207c06a58ee37939e34dd0
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-19 15:53:25 +0200

    feat: add LoopStep

---
+++ src/parser/steps/loop_step.rs
@@ -0,0 +1,5 @@
+use crate::parser::steps::json_parsing_step::JsonParsingStep;
+
+pub trait LoopStep: JsonParsingStep {
+    
+}
--- src/parser/steps/mod.rs
+++ src/parser/steps/mod.rs
@@ -1,2 +1,3 @@
 pub mod block_step;
 pub mod json_parsing_step;
+pub mod loop_step;
