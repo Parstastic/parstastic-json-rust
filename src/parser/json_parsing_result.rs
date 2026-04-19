2ce121eff56fe4ba95526a15a62836e5a234b236
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-19 15:44:46 +0200

    feat: add JsonParsingResult

---
+++ src/parser/json_parsing_result.rs
@@ -0,0 +1,66 @@
+use crate::{
+    node::json_particle::JsonParticle,
+    parser::json_parsing_process::JsonParsingProcess
+};
+
+pub struct JsonParsingResultError {
+    message: String,
+    parsing_process: JsonParsingProcess
+}
+
+impl JsonParsingResultError {
+    pub fn new(message: String, parsing_process: JsonParsingProcess) -> Self {
+        Self {
+            message,
+            parsing_process
+        }
+    }
+}
+
+
+pub enum JsonParsingResult<T: JsonParticle> {
+    Value(T),
+    Error(JsonParsingResultError)
+}
+
+impl<T: JsonParticle> JsonParsingResult<T> {
+    pub fn with_value(value: T) -> Self {
+        Self::Value(value)
+    }
+
+    pub fn with_error(error: JsonParsingResultError) -> Self {
+        Self::Error(error)
+    }
+
+    pub fn with_error_to_create(message: String, parsing_process: JsonParsingProcess) -> Self {
+        Self::with_error(JsonParsingResultError::new(message, parsing_process))
+    }
+
+    pub fn has_value(&self) -> bool {
+        match self {
+            JsonParsingResult::Value(_) => true,
+            JsonParsingResult::Error(_) => false,
+        }
+    }
+
+    pub fn get_value(&self) -> Option<&T> {
+        match self {
+            JsonParsingResult::Value(value) => Some(value),
+            JsonParsingResult::Error(_) => None,
+        }
+    }
+
+    pub fn has_error(&self) -> bool {
+        match self {
+            JsonParsingResult::Value(_) => false,
+            JsonParsingResult::Error(_) => true,
+        }
+    }
+
+    pub fn get_error(&self) -> Option<&JsonParsingResultError> {
+        match self {
+            JsonParsingResult::Value(_) => None,
+            JsonParsingResult::Error(error) => Some(error),
+        }
+    }
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,3 +2,4 @@ pub mod parsers;
 pub mod steps;
 
 pub mod json_parsing_process;
+pub mod json_parsing_result;
