d50c10705d78e8bbd6ee0361619c76ae15f68e92
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-19 22:36:19 +0200

    feat: add JsonValueParser

---
+++ src/parser/parsers/json_value_parser.rs
@@ -0,0 +1,51 @@
+use crate::{
+    node::{
+        json_value::JsonValue, 
+        nodes::json_node::JsonNode, 
+        whitespace::Whitespace
+    },
+    parser::{
+        parsers::json_particle_parser::JsonParticleParser, 
+        steps::block_step::BlockStep
+    }
+};
+
+pub struct JsonValueParser {
+    leading_whitespace: Option<Whitespace>,
+    json_node: Option<Box<dyn JsonNode>>,
+    trailing_whitespace: Option<Whitespace>,
+}
+
+impl JsonValueParser {
+    pub fn new() -> Self {
+        Self {
+            leading_whitespace: None,
+            json_node: None,
+            trailing_whitespace: None,
+        }
+    }
+
+    pub fn new_with_leading_whitespace(leading_whitespace: Whitespace) -> Self {
+        Self {
+            leading_whitespace: Some(leading_whitespace),
+            json_node: None,
+            trailing_whitespace: None,
+        }
+    }
+}
+
+impl JsonParticleParser<JsonValue> for JsonValueParser {
+    type Step = BlockStep<3, JsonValue, Self>;
+
+    fn can_parse(&self, parsing_process: &crate::parser::json_parsing_process::JsonParsingProcess) -> bool {
+        todo!()
+    }
+
+    fn get_step(&mut self) -> Self::Step {
+        todo!()
+    }
+
+    fn create(&self) -> JsonValue {
+        todo!()
+    }
+}
--- src/parser/parsers/mod.rs
+++ src/parser/parsers/mod.rs
@@ -1,4 +1,5 @@
 pub mod nodes;
 
 pub mod json_particle_parser;
+pub mod json_value_parser;
 pub mod whitespace_parser;
