2e476ef45128fa941a98ed4aa8dcb842bdb7b59e
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-19 23:02:51 +0200

    feat: add NullNodeParser

--- src/parser/parsers/nodes/mod.rs
+++ src/parser/parsers/nodes/mod.rs
@@ -1,1 +1,2 @@
 pub mod json_node_parser;
+pub mod null_node_parser;
---
+++ src/parser/parsers/nodes/null_node_parser.rs
@@ -0,0 +1,52 @@
+use std::array::from_fn;
+
+use crate::{
+    node::{
+        nodes::null_node::{
+            NULL_NODE, NullNode, STRING_VALUE
+        }
+    },
+    parser::{
+        parsers::json_particle_parser::JsonParticleParser,
+        steps::{block_step::BlockStep, json_parsing_step::JsonParsingStep, parse_character_step::ParseCharacterStep}
+    }
+};
+
+const N: usize = STRING_VALUE.len();
+
+pub struct NullNodeParser {
+
+}
+
+impl NullNodeParser {
+    pub fn new() -> Self {
+        Self {
+
+        }
+    }
+
+    fn create_validation(&self) -> [Box<dyn JsonParsingStep<NullNode, Self>>; N] {
+        let mut iter = STRING_VALUE.chars();
+        from_fn(|_| {
+            Box::new(
+                ParseCharacterStep::new_with_expected_character(iter.next().unwrap())
+            ) as Box<dyn JsonParsingStep<NullNode, Self>>
+        })
+    }
+}
+
+impl JsonParticleParser<NullNode> for NullNodeParser {
+    type Step = BlockStep<N, NullNode, Self>;
+
+    fn can_parse(&self, parsing_process: &crate::parser::json_parsing_process::JsonParsingProcess) -> bool {
+        parsing_process.starts_with(STRING_VALUE)
+    }
+
+    fn get_step(&mut self) -> Self::Step {
+        BlockStep::new(self.create_validation())
+    }
+
+    fn create(&self) -> NullNode {
+        NULL_NODE
+    }
+}
