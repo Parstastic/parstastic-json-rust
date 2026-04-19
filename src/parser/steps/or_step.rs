841f6fe37db90cdca526cf1a9e5b760d3e95b1d5
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-19 21:25:34 +0200

    refactor: invert ifSteps Map in OrStep

--- src/parser/steps/or_step.rs
+++ src/parser/steps/or_step.rs
@@ -10,13 +10,13 @@ use crate::parser::{
 };
 
 pub struct OrStep {
-    if_steps: HashMap<Box<dyn JsonParsingStep>, Box<dyn Fn(&JsonParsingProcess) -> bool>>,
+    if_steps: HashMap<Box<dyn Fn(&JsonParsingProcess) -> bool>, Box<dyn JsonParsingStep>>,
     else_step: Box<dyn JsonParsingStep>
 }
 
 impl OrStep {
     pub fn new(
-        if_steps: HashMap<Box<dyn JsonParsingStep>, Box<dyn Fn(&JsonParsingProcess) -> bool>>,
+        if_steps: HashMap<Box<dyn Fn(&JsonParsingProcess) -> bool>, Box<dyn JsonParsingStep>>,
         else_step: Box<dyn JsonParsingStep>
     ) -> Self {
         Self {
@@ -25,14 +25,14 @@ impl OrStep {
         }
     }
 
-    pub fn else_error(if_steps: HashMap<Box<dyn JsonParsingStep>, Box<dyn Fn(&JsonParsingProcess) -> bool>>) -> Self {
+    pub fn else_error(if_steps: HashMap<Box<dyn Fn(&JsonParsingProcess) -> bool>, Box<dyn JsonParsingStep>>) -> Self {
         Self::new(
             if_steps,
             Box::new(ExportStep::new(|| false))
         )
     }
 
-    pub fn else_success(if_steps: HashMap<Box<dyn JsonParsingStep>, Box<dyn Fn(&JsonParsingProcess) -> bool>>) -> Self {
+    pub fn else_success(if_steps: HashMap<Box<dyn Fn(&JsonParsingProcess) -> bool>, Box<dyn JsonParsingStep>>) -> Self {
         Self::new(
             if_steps,
             Box::new(ExportStep::new(|| true))
@@ -43,8 +43,8 @@ impl OrStep {
 impl JsonParsingStep for OrStep {
     fn execute(&self, parsing_process: &mut JsonParsingProcess) -> Option<JsonParsingResultError> {
         for if_step in self.if_steps.iter() {
-            if (if_step.1)(parsing_process) {
-                return if_step.0.execute(parsing_process);
+            if (if_step.0)(parsing_process) {
+                return if_step.1.execute(parsing_process);
             }
         }
         self.else_step.execute(parsing_process)
