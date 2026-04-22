115a958550a6ed81a5352301cafd28ef19724598
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-22 23:44:09 +0200

    feat: add StringNodeParser

--- src/parser/parsers/nodes/mod.rs
+++ src/parser/parsers/nodes/mod.rs
@@ -2,3 +2,4 @@ pub mod boolean_node_parser;
 pub mod json_node_parser;
 pub mod null_node_parser;
 pub mod number_node_parser;
+pub mod string_node_parser;
---
+++ src/parser/parsers/nodes/string_node_parser.rs
@@ -0,0 +1,131 @@
+use crate::{
+    node::nodes::string_node::{
+        DELIMITER, 
+        StringNode
+    },
+    parser::{
+        json_parsing_process::JsonParsingProcess,
+        parsers::{
+            json_particle_parser::JsonParticleParser,
+            nodes::json_node_parser::JsonNodeParser
+        },
+        steps::{
+            block_step::BlockStep,
+            for_loop_step::ForLoopStep,
+            json_parsing_step::JsonParsingStep,
+            or_step::OrStep,
+            parse_character_step::ParseCharacterStep,
+            validate_character_step::ValidateCharacterStep,
+            while_loop_step::WhileLoopStep
+        }
+    }
+};
+
+pub struct StringNodeParser {
+    characters: Vec<char>
+}
+
+impl StringNodeParser {
+    pub fn new() -> Self {
+        Self {
+            characters: Vec::new()
+        }
+    }
+
+    fn create_add_character_step(&self) -> ParseCharacterStep<StringNode, Self> {
+        ParseCharacterStep::new(|p: &mut Self, c| {
+            p.characters.push(c);
+            true
+        })
+    }
+
+    fn create_delimiter_parsing_step(&self) -> ParseCharacterStep<StringNode, Self> {
+        ParseCharacterStep::new_with_expected_character(DELIMITER)
+    }
+
+    fn create_characters_parsing_step(&self) -> WhileLoopStep<OrStep<StringNode, Self>, StringNode, Self> {
+        WhileLoopStep::new(
+            self.create_character_parsing_step(),
+            |p| !p.is_at_char(DELIMITER)
+        )
+    }
+
+    fn create_character_parsing_step(&self) -> OrStep<StringNode, Self> {
+        OrStep::new(
+            vec![(
+                Box::new(|p| p.is_at_char('\\')),
+                Box::new(BlockStep::new([
+                    Box::new(self.create_add_character_step()),
+                    Box::new(self.create_escape_targets_parsing_step()),
+                ]))
+            )],
+            Box::new(self.create_add_character_step())
+        )
+    }
+
+    fn create_escape_targets_parsing_step(&self) -> OrStep<StringNode, Self> {
+        OrStep::new(
+            self.create_escape_targets_parser_map(),
+            self.create_unicode_parser()
+        )
+    }
+
+    fn create_escape_targets_parser_map(&self) -> Vec<(Box<dyn Fn(&JsonParsingProcess) -> bool>, Box<dyn JsonParsingStep<StringNode, Self>>)> {
+        vec![
+            '"',
+            '\\',
+            '/',
+            'b',
+            'f',
+            'n',
+            'r',
+            't'
+        ]
+            .into_iter()
+            .map(|c| {
+                let condition: Box<dyn Fn(&JsonParsingProcess) -> bool> = Box::new(move |p: &JsonParsingProcess| p.is_at_char(c));
+                let effect: Box<dyn JsonParsingStep<StringNode, Self>> = Box::new(self.create_add_character_step());
+
+                (condition, effect)
+            })
+            .collect()
+    }
+
+    fn create_unicode_parser(&self) -> Box<dyn JsonParsingStep<StringNode, Self>> {
+        Box::new(BlockStep::new([
+            Box::new(ValidateCharacterStep::new_with_expected_character('u')),
+            Box::new(self.create_add_character_step()),
+            Box::new(ForLoopStep::new(
+                BlockStep::new([
+                    Box::new(ValidateCharacterStep::new(|c| c.is_ascii_hexdigit())),
+                    Box::new(self.create_add_character_step()),
+                ]),
+                4
+            ))
+        ]))
+    }
+}
+
+impl JsonNodeParser<StringNode> for StringNodeParser {
+
+}
+
+impl JsonParticleParser<StringNode> for StringNodeParser {
+    type Step = BlockStep<3, StringNode, Self>;
+
+    fn can_parse(&self, parsing_process: &JsonParsingProcess) -> bool {
+        parsing_process.is_at_char(DELIMITER)
+    }
+
+    fn get_step(&mut self) -> Self::Step {
+        BlockStep::new([
+            Box::new(self.create_delimiter_parsing_step()),
+            Box::new(self.create_characters_parsing_step()),
+            Box::new(self.create_delimiter_parsing_step()),
+        ])
+    }
+
+    fn create(&self) -> Option<StringNode> {
+        Some(StringNode::new(self.characters.iter().collect()))
+    }
+}
