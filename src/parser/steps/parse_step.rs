4d1dbcf2379515ee062935a9f346a6182fd91f06
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-04-25 02:50:54 +0200

    refactor: update ParseStep api to include current parser in creators and add flexibility to next_creator

--- src/parser/steps/parse_step.rs
+++ src/parser/steps/parse_step.rs
@@ -14,41 +14,43 @@ use crate::{
 };
 
 pub struct ParseStep<
-    T: JsonParticle, P: JsonParticleParser<T>, S: JsonParsingStep<JP, JPP>,
+    T: JsonParticle, P: JsonParticleParser<T>,
     JP: JsonParticle, JPP: JsonParticleParser<JP>
 > {
     _jp: PhantomData<JP>,
     _jpp: PhantomData<JPP>,
-    parser_creator: Box<dyn Fn() -> P>,
-    next_creator: Box<dyn Fn(T) -> S>
+    parser_creator: Box<dyn Fn(&mut JPP) -> P>,
+    next_creator: Box<dyn Fn(T, &mut JPP, &mut JsonParsingProcess) -> Option<JsonParsingResultError>>
 }
 
 impl<
-    T: JsonParticle, P: JsonParticleParser<T>, S: JsonParsingStep<JP, JPP>,
+    T: JsonParticle, P: JsonParticleParser<T>,
     JP: JsonParticle, JPP: JsonParticleParser<JP>
-> ParseStep<T, P, S, JP, JPP> {
-    pub fn new<F>(parser_creator: Box<dyn Fn() -> P>, next_creator: F) -> Self
-        where F: Fn(T) -> S + 'static
+> ParseStep<T, P, JP, JPP> {
+    pub fn new<F1, F2>(parser_creator: F1, next_creator: F2) -> Self
+        where
+            F1: Fn(&mut JPP) -> P + 'static,
+            F2: Fn(T, &mut JPP, &mut JsonParsingProcess) -> Option<JsonParsingResultError> + 'static
     {
         Self {
             _jp: PhantomData,
             _jpp: PhantomData,
-            parser_creator,
+            parser_creator: Box::new(parser_creator),
             next_creator: Box::new(next_creator)
         }
     }
 }
 
 impl<
-    T: JsonParticle, P: JsonParticleParser<T>, S: JsonParsingStep<JP, JPP>,
+    T: JsonParticle, P: JsonParticleParser<T>,
     JP: JsonParticle, JPP: JsonParticleParser<JP>
-> JsonParsingStep<JP, JPP> for ParseStep<T, P, S, JP, JPP> {
+> JsonParsingStep<JP, JPP> for ParseStep<T, P, JP, JPP> {
     fn execute(&self, parser: &mut JPP, parsing_process: &mut JsonParsingProcess) -> Option<JsonParsingResultError> {
-        let new_parser = (self.parser_creator)();
+        let new_parser = (self.parser_creator)(parser);
         if new_parser.can_parse(parsing_process) {
             let result = new_parser.parse(parsing_process);
             match result {
-                JsonParsingResult::Value(value) => (self.next_creator)(value).execute(parser, parsing_process),
+                JsonParsingResult::Value(value) => (self.next_creator)(value, parser, parsing_process),
                 JsonParsingResult::Error(_) => Some(JsonParsingResultError::new(
                     "An error occurred during usage of the parser.".to_string(),
                     parsing_process.clone()
